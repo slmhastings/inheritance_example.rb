cdca88e9697a4e20a03c64c55e306ad6f60056d4
Sequona Hastings <slmhastings@gmail.com>
2020-06-23 20:44:55 -0500

    create manager

--- store_item.rb
+++ store_item.rb
@@ -9,9 +9,8 @@
 # ice_cream3 = {:flavor => "pistachio", :size => "1 scoop", price: 2}
 # ice_cream2 = {:flavor => "sweet cream", :size => gallon, price: 10}
 
-class IceCream
+class Ice_Cream < Food
   
-
   def initialize(input_options) 
     @flavor = input_options[:flavor]
     @size = input_options[:size]
@@ -31,6 +30,17 @@ class IceCream
   end
 end
 
-ice_cream1 = IceCream.new(flavor: "chocolate", size: "pint", price: 5)
-ice_cream2 = IceCream.new("sweet cream", "gallon", 10)
-p ice_cream1
+class Food 
+  def initialize(number)
+    @number = number
+  end
+
+  def shelf_life
+    puts "This item is good for #{number} days."
+  end
+end
+
+ice_cream = Ice_Cream.new({flavor: "chocolate", size: "pint", price: 5})
+# IceCream.new(flavor:"sweet cream", size: "gallon", price: 10)
+
+p ice_cream.shelf_life(5)
